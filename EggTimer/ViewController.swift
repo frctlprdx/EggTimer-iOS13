66733d01af7f54817f355cd1719d0ee96aa17548
Ivan Putra Pratama <ifam4@yahoo.com>
2024-03-27 07:39:35 +0700

    Egg Timer app
    training project to countdown egg based on input and time to cook it until cooked perfectly

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = [
+        "Soft" : 300,
+        "Medium" : 420,
+        "Hard" : 7200
+    ]
+    
+    var value = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        value = eggTimes[hardness]!
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if value > 0{
+            print("\(value)s")
+            value -= 1
+        }
+    }
 }
